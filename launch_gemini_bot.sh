0b2e2ab85670ade40b62bc2e5af0024d683787f9
Ren Xiang <foorenxiang@gmail.com>
2022-07-04 23:48:01 +0800

    add shell script for running bot

---
+++ launch_gemini_bot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+OUTPUT_LOG="gemini_bot.log"
+PROGRAM_DIR="~/gemini-hybrid-dca-dip-buying-bot"
+cd $PROGRAM_DIR
+rm gemini_bot.log
+nohup python3 __main__.py >$OUTPUT_LOG &
+tail -f $OUTPUT_LOG
