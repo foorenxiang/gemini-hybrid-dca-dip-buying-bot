105fec2230114adca3a96e7c7df5cd62114ac430
Ren Xiang <foorenxiang@gmail.com>
2022-07-05 02:12:33 +0800

    simplify trade module

--- bot/trade.py
+++ bot/trade.py
@@ -1,5 +1,4 @@
-from typing import Optional, Tuple, Union
-from pprint import pprint
+from typing import Tuple
 from bot.models import GeminiOrder
 from bot.heartbeat import handle_heartbeat
 from bot.actions import (
@@ -10,16 +9,7 @@ from bot.heuristics import is_to_make_market_order, is_to_create_limit_orders
 from bot import config
 
 
-def trade_loop():
-    handle_heartbeat()
-    if is_to_make_market_order():
-        new_market_order: GeminiOrder = make_tkn_market_order(
-            tkn_pair="ethsgd",
-            tkn_b_sell_qty=config.dca_amount_per_transaction,
-        )
-        print("Created market order")
-        print(new_market_order)
-
+def handle_limit_orders():
     limit_order_price_levels: Tuple[float] = is_to_create_limit_orders()
     if limit_order_price_levels:
         for tkn_a_limit_price in limit_order_price_levels:
@@ -32,17 +22,23 @@ def trade_loop():
             print(new_limit_order)
 
 
-def trade(
-    dca_amount: Optional[Union[int, float]] = None,
-    max_tkn_b_market_price_in_tkn_a: Optional[Union[int, float]] = None,
-):
-    if isinstance(dca_amount, (int, float)) and dca_amount:
-        config.dca_amount_per_transaction = dca_amount
+def handle_market_orders():
+    if is_to_make_market_order():
+        new_market_order: GeminiOrder = make_tkn_market_order(
+            tkn_pair="ethsgd",
+            tkn_b_sell_qty=config.dca_amount_per_transaction,
+        )
+        print("Created market order")
+        print(new_market_order)
 
-    if max_tkn_b_market_price_in_tkn_a and isinstance(
-        max_tkn_b_market_price_in_tkn_a, (int, float)
-    ):
-        config.max_tkn_b_market_price_in_tkn_a = max_tkn_b_market_price_in_tkn_a
+
+def trade_loop():
+    handle_heartbeat()
+    handle_market_orders()
+    handle_limit_orders()
+
+
+def trade():
     while True:
         trade_loop()
 
