d751671074c2e5c052cc5e94172653bd23a569be
Ren Xiang <foorenxiang@gmail.com>
2022-07-09 09:48:51 +0800

    wrap order making logic with exception handlers

--- bot/trade.py
+++ bot/trade.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+import logging
 from bot.models import GeminiOrder
 from bot.heartbeat import handle_heartbeat
 from bot.actions import (
@@ -10,30 +11,41 @@ from bot.heuristics import is_to_make_market_order, is_to_create_limit_orders
 from bot import config
 import time
 
+logger = logging.getLogger(__name__)
+
 
 def handle_market_orders():
-    if is_to_make_market_order():
-        new_market_order: GeminiOrder = make_tkn_market_order(
-            tkn_pair="ethsgd",
-            tkn_b_sell_qty=config.dca_amount_per_transaction,
-        )
-        print("Created market order")
-        print(new_market_order)
+    try:
+        if is_to_make_market_order():
+            new_market_order: GeminiOrder = make_tkn_market_order(
+                tkn_pair="ethsgd",
+                tkn_b_sell_qty=config.dca_amount_per_transaction,
+            )
+            print("Created market order")
+            print(new_market_order)
+    except Exception:
+        logger.exception("Error while handling market orders")
 
 
 def handle_limit_orders():
-    limit_order_price_levels: Tuple[float] = is_to_create_limit_orders()
-    if limit_order_price_levels:
-        tkn_pair = "ETHSGD"
-        for tkn_a_limit_price in limit_order_price_levels:
-            tkn_b_sell_quantity: float = config.stop_limit_amount_per_stop_limit_order
-            new_limit_order: GeminiOrder = make_tkn_limit_order(
-                tkn_pair=tkn_pair,
-                tkn_b_sell_qty=tkn_b_sell_quantity,
-                tkn_a_buy_price=tkn_a_limit_price,
-            )
-            print("Created limit order")
-            print(new_limit_order)
+    try:
+        limit_order_price_levels: Tuple[float] = is_to_create_limit_orders()
+        if limit_order_price_levels:
+            tkn_pair = "ETHSGD"
+            for tkn_a_limit_price in limit_order_price_levels:
+                tkn_b_sell_quantity: float = (
+                    config.stop_limit_amount_per_stop_limit_order
+                )
+                new_limit_order: GeminiOrder = make_tkn_limit_order(
+                    tkn_pair=tkn_pair,
+                    tkn_b_sell_qty=tkn_b_sell_quantity,
+                    tkn_a_buy_price=tkn_a_limit_price,
+                )
+                print("Created limit order")
+                print(new_limit_order)
+    except Exception:
+        logger.exception("Error while handling limit orders")
+        cancel_session_orders()
 
 
 def slow_down_loop():
